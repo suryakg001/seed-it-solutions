617d80ea0c0cf17e7184cb0da0b13b5bad3af183
Surya@ <suryakg001@gmail.com>
2026-09-22 18:47:37 +0530

    Solve: [Q0.3] Basic_level_0_Datatypes_3 (Medium) - C | SEED-IT

---
+++ Problems/general/q0-3-basic-level-0-datatypes-3/solution.c
@@ -0,0 +1,17 @@
+/**
+ * Problem: Q0.3 - Basic_level_0_Datatypes_3
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-22
+ * Language: c
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("%%");
+    printf("%%");
+    return 0;
+}
