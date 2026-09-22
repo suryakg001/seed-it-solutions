c9af2a714b99a7321c31480f7166e1cebb25425f
Surya@ <suryakg001@gmail.com>
2026-09-22 18:51:10 +0530

    Solve: [Q0.4] Basic_level_0_Datatypes_4 (Medium) - C | SEED-IT

---
+++ Problems/general/q0-4-basic-level-0-datatypes-4/solution.c
@@ -0,0 +1,17 @@
+/**
+ * Problem: Q0.4 - Basic_level_0_Datatypes_4
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-22
+ * Language: c
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("\\");
+
+    return 0;
+}
